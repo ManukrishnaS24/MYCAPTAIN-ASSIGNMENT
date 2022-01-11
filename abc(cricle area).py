ec17d02322fc8dfb2c25dfe566c5b803d82ed387
ManukrishnaS24 <97109253+ManukrishnaS24@users.noreply.github.com>
2022-01-11 08:22:36 +0400

    Update abc(cricle area).py

--- abc(cricle area).py
+++ abc(cricle area).py
@@ -1,3 +1,6 @@
 radius=float(input("Input the radius of the cricle : "))
 from math import pi
 print("The area of the circle with radius ",radius," is: ",pi*radius**2)
+import os
+filename, file_extension = os.path.splitext('C:/Users/sadiy/Downloads/MYCAPTAIN-ASSIGNMENT/abc(cricle area).py')
+print(file_extension)
